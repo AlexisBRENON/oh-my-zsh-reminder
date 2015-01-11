6c2b3f7d7b13b5301aa2d6a96f4d4213363dcf56
Alexis BRENON <brenon.alexis@gmail.com>
2015-01-11 21:00:24 +0100

    Increase display

--- reminder.plugin.zsh
+++ reminder.plugin.zsh
@@ -1,19 +1,34 @@
+touch ~/.todo_list
+colors
+todo_color=(red green yellow blue magenta cyan)
+color_index=1
+
 function todo {
-    echo "  - $@" >> ~/.todo_list
+    task=$(echo -E "$@" | tr '\n' '\r' | sed -e 's/\r$//' -e 's/\r/\n    /g')
+    task="$task\x1E"
+    echo "  - ${fg[${todo_color[${color_index}]}]}$task${fg[default]}" >> ~/.todo_list
+    (( color_index %= ${#todo_color} ))
+    (( color_index += 1 ))
 }
 
 function task_done {
-    grep -v -E -i -e "^  - $1" ~/.todo_list > /tmp/todo_list
+    pattern="$1"
+    touch /tmp/todo_list
+    IFS=''
+    while read line
+    do
+        if [[ ! "${line}" =~ "${pattern}" ]] then
+            echo "$line" >> /tmp/todo_list
+        fi
+    done < ~/.todo_list
     mv -f /tmp/todo_list ~/.todo_list
 }
 
 function display_todo {
     if [[ -s ~/.todo_list ]] then
-        echo "Todo :"
+        echo "${fg_bold[default]}Todo :${fg_no_bold[default]}"
         cat ~/.todo_list
     fi
 }
-
-touch ~/.todo_list
 
 precmd_functions+=(display_todo)
