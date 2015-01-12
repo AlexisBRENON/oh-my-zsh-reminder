59ac965e941b8bf85a20e4f6fab4d450ebbf6c76
Alexis BRENON <brenon.alexis@gmail.com>
2015-01-12 15:55:12 +0100

    Use more ZSH features instead of classic SH
    This new implementation keep task list in memory and only write to disk when ZSH exit

--- reminder.plugin.zsh
+++ reminder.plugin.zsh
@@ -1,34 +1,54 @@
-touch ~/.todo_list
+TODO_SAVE_TASKS_FILE="$HOME/.todo.sav"
+TODO_SAVE_COLOR_FILE="$HOME/.todo_color.sav"
+
+# Allow to use colors
 colors
-todo_color=(red green yellow blue magenta cyan)
-color_index=1
+typeset -T -x -g TODO_TASKS todo_tasks
+typeset -a -x -g todo_colors
+typeset -i -x -g todo_color_index
 
-function todo {
-    task=$(echo -E "$@" | tr '\n' '\r' | sed -e 's/\r$//' -e 's/\r/\n    /g')
-    task="$task\x1E"
-    echo "  - ${fg[${todo_color[${color_index}]}]}$task${fg[default]}" >> ~/.todo_list
-    (( color_index %= ${#todo_color} ))
-    (( color_index += 1 ))
-}
+# Load previous tasks from saved file
+if [[ -e "$TODO_SAVE_TASKS_FILE" &&
+      -e "$TODO_SAVE_COLOR_FILE" ]] then
+    TODO_TASKS="$(cat $TODO_SAVE_TASKS_FILE)"
+    todo_color_index="$(cat $TODO_SAVE_COLOR_FILE)"
+    if [[ -z "$TODO_TASKS" ]] then
+        todo_tasks[1]=()
+    fi
+else
+    todo_tasks=()
+    todo_color_index=1
+fi
+todo_colors=(red green yellow blue magenta cyan)
 
-function task_done {
-    pattern="$1"
-    touch /tmp/todo_list
-    IFS=''
-    while read line
-    do
-        if [[ ! "${line}" =~ "${pattern}" ]] then
-            echo "$line" >> /tmp/todo_list
-        fi
-    done < ~/.todo_list
-    mv -f /tmp/todo_list ~/.todo_list
-}
+precmd_functions+=(todo_display)
+zshexit_functions+=(todo_save)
 
-function display_todo {
-    if [[ -s ~/.todo_list ]] then
-        echo "${fg_bold[default]}Todo :${fg_no_bold[default]}"
-        cat ~/.todo_list
+function todo_add_task {
+    if [[ $# -gt 0 ]] then
+      task=$(echo -E "$@" | tr '\n' '\r' | sed -e 's/\r$//' -e 's/\r/\n    /g')
+      task="  - ${fg[${todo_colors[${todo_color_index}]}]}\x1E$task\x1E$fg[default]"
+      todo_tasks+="$task"
+      (( todo_color_index %= ${#todo_colors} ))
+      (( todo_color_index += 1 ))
     fi
 }
 
-precmd_functions+=(display_todo)
+function todo_task_done {
+    pattern="$1"
+    todo_tasks[${(M)todo_tasks[(i)*\x1E*${pattern}*\x1E*]}]=()
+}
+
+function todo_display {
+    if [[ ${#todo_tasks} -gt 0 ]] then
+      print -l "$fg_bold[default]Todo :$fg_no_bold[default]" ${todo_tasks}
+    fi
+}
+
+function todo_save {
+    echo "$TODO_TASKS" > $TODO_SAVE_TASKS_FILE
+    echo "$todo_color_index" > $TODO_SAVE_COLOR_FILE
+}
+
+alias todo=todo_add_task
+alias task_done=todo_task_done
